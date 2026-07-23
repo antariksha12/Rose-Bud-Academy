ebb391741203d6db06ff03c2ffc15aeda0db652f
antariksha12 <antariksha490@gmail.com>
2026-07-23 10:02:38 +0530

    Add scroll to top navigation

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,26 @@ import Contact from './pages/Contact.jsx'
 import JoinOurTeam from './pages/JoinOurTeam.jsx'
 import PrivacyPolicy from './pages/PrivacyPolicy.jsx'
 import TermsAndConditions from './pages/TermsAndConditions.jsx'
+import ScrollToTop from './components/ScrollToTop.jsx'
 
 function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-      <Route path="/about" element={<About />} />
-      <Route path="/academics" element={<Academics />} />
-      <Route path="/student-life" element={<StudentLife />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/join-our-team" element={<JoinOurTeam />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-<Route path="/terms-and-conditions" element={<TermsAndConditions />} />
-      </Route>
-    </Routes>
+    <>
+      <ScrollToTop />
+
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/academics" element={<Academics />} />
+          <Route path="/student-life" element={<StudentLife />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/join-our-team" element={<JoinOurTeam />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+        </Route>
+      </Routes>
+    </>
   )
 }
 
