3205c2732e89ac63b7b26f1aa3931d818d32d85a
antariksha12 <antariksha490@gmail.com>
2026-07-23 12:26:20 +0530

    Remove unnecessary page loader

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,18 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
 
 import App from './App.jsx'
 import './index.css'
-import PageLoader from './components/ui/PageLoader.jsx'
 
 function Root() {
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false)
-    }, 500)
-
-    return () => clearTimeout(timer)
-  }, [])
-
   return (
-    <>
-      {loading && <PageLoader />}
-
-      <HelmetProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </HelmetProvider>
-    </>
+    <HelmetProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </HelmetProvider>
   )
 }
 
